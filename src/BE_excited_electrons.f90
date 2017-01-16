7d1e8a8d42d7bda1b766e690f2065dee9836f787
Shunsuke Sato <shunsuke.sato26@gmail.com>
2017-01-16 11:36:53 +0100

    Output single particle energies.

--- src/BE_excited_electrons.f90
+++ src/BE_excited_electrons.f90
@@ -16,13 +16,15 @@
 subroutine BE_excited_electrons
   use global_variables
   implicit none
-  real(8),allocatable :: occ_t(:,:,:),occ_t_l(:,:,:)
+  real(8),allocatable :: occ_t(:,:,:),occ_t_l(:,:,:),eig_l(:,:),eig(:,:)
   integer :: ik,ib1,ib2
 
   if(myrank == 0)write(*,"(A)")"== Start: Distribution of electron-hole."
 
   allocate(occ_t(NB_basis,NB_TD,NK),occ_t_l(NB_basis,NB_TD,NK))
-  occ_t_l = 0d0
+  allocate(eig_l(NB_basis,NK),eig(NB_basis,NK))
+  occ_t_l = 0d0; eig_l = 0d0
+
 
   do ik = NK_s,NK_e
     do ib1 = 1,NB_TD
@@ -35,9 +37,11 @@ subroutine BE_excited_electrons
   end do
 
   call MPI_ALLREDUCE(occ_t_l,occ_t,NB_basis*NB_TD*NK,MPI_REAL8,MPI_SUM,NEW_COMM_WORLD,ierr)  
+  eig_l(:,NK_s:NK_e) = H0_eigval(:,NK_s:NK_e)
+  call MPI_ALLREDUCE(eig_l,eig,NB_basis*NK,MPI_REAL8,MPI_SUM,NEW_COMM_WORLD,ierr)  
 
   if(myrank == 0)then
-    open(701,file="dist_nex.out")
+    open(701,file="dist_occ.out")
     write(701,"(A,2x,3I7)")"#NB_basis,NB_TD,NK=",NB_basis,NB_TD,NK
     do ik = NK_s,NK_e
       do ib1 = 1,NB_TD
@@ -47,6 +51,16 @@ subroutine BE_excited_electrons
       end do
     end do
     close(701)
+
+    open(701,file="dist_nex.out")
+    write(701,"(A,2x,3I7)")"#NB_basis,NB_TD,NK=",NB_basis,NB_TD,NK
+    do ik = NK_s,NK_e
+      do ib2 = 1,NB_basis
+        write(701,"(999e26.16e3)")eig(ib2,ik),sum(occ_t(ib2,:,ik))
+      end do
+    end do
+    close(701)
+
   end if
 
   if(myrank == 0)write(*,"(A)")"== Start: Distribution of electron-hole."
