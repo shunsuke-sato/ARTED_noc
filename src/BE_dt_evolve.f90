dd8e8a2f04389e0047516496228485a25d3ff966
Shunsuke Sato <shunsuke.sato26@gmail.com>
2018-11-06 11:18:51 +0100

    Optimized propagator.

--- src/BE_dt_evolve.f90
+++ src/BE_dt_evolve.f90
@@ -24,6 +24,81 @@ subroutine BE_dt_evolve(Act_t)
   integer :: ik,ib,iexp
   complex(8) :: zfact
 
+!== construct current matrix start
+  diff = 1d10
+  do iav = -NAmax,NAmax
+    if( abs(Act_t-dble(iav)*dAmax) < diff)then
+      diff = abs(Act_t-dble(iav)*dAmax)
+      iav_t = iav
+    end if
+  end do
+  if(iav_t == NAmax)iav_t=NAmax -1
+  if(iav_t == -NAmax)iav_t=-NAmax +1
+  if(abs(Act_t) > Amax)then
+    err_message='Amax is too small.'
+    call err_finalize
+  end if
+
+  xx = (Act_t-dble(iav_t)*dAmax)/dAmax
+  zH_tot(:,:,:) = 0.5d0*zV_NL(:,:,:,iav_t+1)*(xx**2+xx) &
+    +0.5d0*zV_NL(:,:,:,iav_t-1)*(xx**2-xx) &
+    +      zV_NL(:,:,:,iav_t)*(1d0 - xx**2)
+  zH_tot = zH_tot + zH_loc + zPi_loc*Act_t
+  do ib = 1,NB_basis
+    zH_tot(ib,ib,:) = zH_tot(ib,ib,:) + 0.5d0*Act_t**2
+  end do
+
+  call BE_dt_full_evolve_Taylor
+
+contains
+
+  subroutine BE_dt_full_evolve_Taylor
+    implicit none
+    integer,parameter :: nexp_taylor = 4
+    complex(8) :: zvec0(NB_basis, NB_TD)
+    complex(8) :: zhvec0(NB_basis, NB_TD)
+      
+
+    K_point : do ik=NK_s,NK_e
+      
+      zfact = 1d0
+      zvec0 = zCt(1:NB_basis, 1:NB_TD, ik)
+      do iexp = 1, nexp_taylor
+        zfact = zfact*(-zI*dt)/iexp
+!          zhvec0(1:NB_basis, 1:NB_TD) = matmul(&
+!            zH_tot(1:NB_basis,1:NB_basis,ik),&
+!            zvec0(1:NB_TD))
+        call zhemm('L', 'U', NB_basis, NB_TD, (1d0,0d0), &
+          zH_tot(1:NB_basis,1:NB_basis,ik), &
+          NB_basis,&
+          zvec0(1:NB_basis,1:NB_TD), &
+          NB_basis, (0d0,0d0), &
+          zhvec0(1:NB_basis,1:NB_TD), &
+          NB_basis)
+            
+
+        zCt(1:NB_basis, 1:NB_TD, ik) = zCt(1:NB_basis, 1:NB_TD, ik) &
+          +zfact*zhvec0(1:NB_basis, 1:NB_TD)
+        if(iexp /= nexp_taylor)zvec0 = zhvec0
+
+      end do
+    end do K_point
+
+  end subroutine BE_dt_full_evolve_Taylor
+
+end subroutine BE_dt_evolve
+!---------------------------------------------------------------------------------
+subroutine BE_dt_evolve_old(Act_t)
+  use global_variables
+  use PSE_variables
+  implicit none
+  real(8) :: Act_t
+  real(8),parameter :: eps_Act = 1d-6
+  integer :: iav, iav_t
+  real(8) :: diff,xx
+  integer :: ik,ib,iexp
+  complex(8) :: zfact
+
   if(Act_t == 0d0)then
     ! free propagation
   call BE_dt_evolve_Free
@@ -316,5 +391,5 @@ subroutine BE_dt_evolve(Act_t)
       end do K_point
     end subroutine BE_dt_full_evolve_Lanczos
 
-end subroutine BE_dt_evolve
+  end subroutine BE_dt_evolve_old
   
